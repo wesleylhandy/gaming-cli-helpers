37d0b9c40250fa83cecc5ad204adbee2b77f93d4
Wesley L. Handy <wesleylhandy@gmail.com>
2020-03-24 14:06:48 -0400

    add dice display

--- dice.js
+++ dice.js
@@ -24,11 +24,18 @@ class Dice {
 
   rollDice = async () => {
     let promises = [];
+    console.clear();
+    // const newLines = this.dice.length * 6 + 5
+    // for (let lines = 0; lines < newLines; lines ++) {
+    //   console.log("");
+    // }
+    // process.stdout.moveCursor(0, newLines * -1);
     for (let i = 0; i < this.dice.length; i++) {
       promises.push(this.dice[i].rollDie(i));
     }
     try {
       const results = await Promise.all(promises);
+      process.stdout.cursorTo(0, this.dice.length * 6);
       console.log("----------");
       console.log(
         "Total Roll: ",
--- die.js
+++ die.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 class Die {
   constructor(sides) {
     if (!this instanceof Die) {
@@ -7,6 +9,15 @@ class Die {
     this.counter = this.randomize(1, 20);
     this.interval = this.randomize(16, 75);
     this.weight = 4.1;
+    
+  }
+
+  getDisplay = (roll) => {
+    const yMargin = '         ';
+    const row1 = roll > 3 ? '\u2022   \u2022' : roll > 1 ? '\u2022    ' : '     ';
+    const row2 = roll == 6 ? '\u2022   \u2022' : roll % 2 == 1 ? '  \u2022  ' : '     ';
+    const row3 = roll > 3 ? '\u2022   \u2022' : roll > 1 ? '    \u2022' : '     ';
+    return chalk.black.bold.bgWhiteBright(`${yMargin}\n  ${[row1 , row2, row3].join('  \n  ')}  \n${yMargin}\n`);
   }
 
   randomize = (min = 1, max) =>
@@ -15,10 +26,16 @@ class Die {
   rollDie = i =>
     new Promise(resolve => {
       const getRollSnapshot = () => {
+        const rollValue = this.randomize(1, this.sides);
+        if (this.sides === 6) {
+          const display = this.getDisplay(rollValue);
+          process.stdout.cursorTo(0, i * 6);
+          process.stdout.write(display);
+        }
         if (this.counter == 0) {
-          const returnVal = this.randomize(1, this.sides);
-          console.log(`${i + 1} - ${this.sides}-sided Die: `, returnVal);
-          return resolve(returnVal);
+          process.stdout.cursorTo(0, (i * 6) + 5);
+          console.log(`${i + 1} - ${this.sides}-sided Die: ${rollValue}`);
+          return resolve(rollValue);
         }
         this.counter--;
         setTimeout(getRollSnapshot, this.interval);
